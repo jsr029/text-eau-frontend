4a72715bcbfa29cf9c2bb1a07b0a286ef29bf4fe
jeanSebastienRakotonirina <jsr029.applyforjob@gmail.com>
2026-07-19 21:05:20 +0200

    rt

--- src/components/OrdersManagement.jsx
+++ src/components/OrdersManagement.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 const OrdersManagement = ({ user }) => {
   const [orders, setOrders] = useState([]);
   const [newOrder, setNewOrder] = useState({ 
